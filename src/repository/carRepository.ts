d8be251b06cf3e29dbac6d61933af873be637d3b
JeffMota <jmota906@gmail.com>
2023-04-13 17:01:19 -0300

    final result

--- src/repository/carRepository.ts
+++ src/repository/carRepository.ts
@@ -1,19 +1,27 @@
 import prisma from '../config/database.js'
-import { cars } from '@prisma/client';
 
 async function getCars() {
-  console.log('aqui')
   return await prisma.cars.findMany();
 }
 
 async function getCar(id: number) {
   // const data = await db.query(`SELECT * FROM cars WHERE id = $1`, [id]);
   // return data.rows[0];
+  return await prisma.cars.findFirst({
+    where: {
+      id,
+    },
+  })
 }
 
 async function getCarWithLicensePlate(licensePlate: string) {
   // const data = await db.query(`SELECT * FROM cars WHERE "licensePlate" = $1`, [licensePlate]);
   // return data.rows[0];
+  return await prisma.cars.findFirst({
+    where: {
+      licensePlate,
+    }
+  })
 }
 
 async function createCar(model: string, licensePlate: string, year: number, color: string) {
@@ -22,10 +30,23 @@ async function createCar(model: string, licensePlate: string, year: number, colo
   //    VALUES ($1, $2, $3, $4)`,
   //   [model, licensePlate, year, color]
   // );
+  await prisma.cars.create({
+    data: {
+      model,
+      licensePlate,
+      year,
+      color,
+    }
+  })
 }
 
 async function deleteCar(id: number) {
   // await db.query(`DELETE FROM cars WHERE id = $1`, [id]);
+  await prisma.cars.delete({
+    where: {
+      id
+    }
+  })
 }
 
 const carRepository = {
--- src/services/carService.ts
+++ src/services/carService.ts
@@ -9,21 +9,21 @@ async function getCars() {
 }
 
 async function getCar(id: number) {
-  // const car = await carRepository.getCar(id);
-  // if (!car) {
-  //   throw notFoundError();
-  // }
+  const car = await carRepository.getCar(id);
+  if (!car) {
+    throw notFoundError();
+  }
 
-  // return car;
+  return car;
 }
 
 async function createCar(model: string, licensePlate: string, year: number, color: string) {
-  // const car = await carRepository.getCarWithLicensePlate(licensePlate);
-  // if (car) {
-  //   throw conflictError(`Car with license plate ${licensePlate} already registered.`)
-  // }
+  const car = await carRepository.getCarWithLicensePlate(licensePlate);
+  if (car) {
+    throw conflictError(`Car with license plate ${licensePlate} already registered.`)
+  }
 
-  // await carRepository.createCar(model, licensePlate, year, color);
+  await carRepository.createCar(model, licensePlate, year, color);
 }
 
 async function deleteCar(id: number) {
